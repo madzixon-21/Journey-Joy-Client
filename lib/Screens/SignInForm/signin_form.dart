e75dc1a8b773ba47c26afa3e43adb01aec871af5
Joanna <joannasovva@gmail.com>
2024-01-14 20:23:36 +0100

    Validate SignIn and fix overflow

---
+++ lib/Screens/SignInForm/signin_form.dart
@@ -0,0 +1,282 @@
+/// # SignIngFomr
+/// ## Form screen used for sign in.
+///
+
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:journey_joy_client/Classes/Functions/signin.dart';
+import 'package:journey_joy_client/Dialogs/error_dialog.dart';
+
+class SignInForm extends StatefulWidget {
+  const SignInForm({super.key});
+
+  @override
+  SignInFormState createState() {
+    return SignInFormState();
+  }
+}
+
+class SignInFormState extends State<SignInForm> {
+  final TextEditingController _sIemailController = TextEditingController();
+  final TextEditingController _sIpasswordController = TextEditingController();
+  final TextEditingController _sIpasswordController2 = TextEditingController();
+  final TextEditingController _sInicknameController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          height: 500,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            image: const DecorationImage(
+              image: AssetImage('assets/small_dialog_background.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            children: [
+              const SizedBox(height: 25),
+              Center(
+                child: Text(
+                  'Create a new account',
+                  style: TextStyle(
+                    color: Colors.grey.shade900,
+                    fontFamily: 'Lohit Tamil',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 35),
+              TextFormField(
+                style: TextStyle(
+                  color: Colors.grey.shade900,
+                  fontFamily: 'Lohit Tamil',
+                  letterSpacing: 2,
+                ),
+                controller: _sInicknameController,
+                maxLength: 50,
+                decoration: InputDecoration(
+                  hintText: 'Username',
+                  hintStyle: TextStyle(
+                    color: Colors.grey.shade900,
+                    fontFamily: 'Lohit Tamil',
+                    letterSpacing: 1.5,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                      vertical: 12.0, horizontal: 12.0),
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.account_circle,
+                    color: Colors.grey.shade900,
+                  ),
+                  prefixIconConstraints: const BoxConstraints(
+                    minWidth: 60,
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter username';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 5),
+              TextFormField(
+                style: TextStyle(
+                  color: Colors.grey.shade900,
+                  fontFamily: 'Lohit Tamil',
+                  letterSpacing: 2,
+                ),
+                maxLength: 50,
+                controller: _sIemailController,
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  hintStyle: TextStyle(
+                    color: Colors.grey.shade900,
+                    fontFamily: 'Lohit Tamil',
+                    letterSpacing: 1.5,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                      vertical: 12.0, horizontal: 12.0),
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.email_rounded,
+                    color: Colors.grey.shade900,
+                  ),
+                  prefixIconConstraints: const BoxConstraints(
+                    minWidth: 60,
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter mail';
+                  } else if (!EmailValidator.validate(value)) {
+                    return 'Please provide valid email address';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 5),
+              TextFormField(
+                key: const Key('passwordTextField'),
+                style: TextStyle(
+                  color: Colors.grey.shade900,
+                  fontFamily: 'Lohit Tamil',
+                  letterSpacing: 2,
+                ),
+                controller: _sIpasswordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  hintStyle: TextStyle(
+                    color: Colors.grey.shade900,
+                    fontFamily: 'Lohit Tamil',
+                    letterSpacing: 1.5,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                      vertical: 12.0, horizontal: 12.0),
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.lock_rounded,
+                    color: Colors.grey.shade900,
+                  ),
+                  prefixIconConstraints: const BoxConstraints(
+                    minWidth: 60,
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter password';
+                  } else if (value.length < 8 || value.length > 25) {
+                    return 'Password length not between 8 and 25 chars.';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                key: const Key('confirmPasswordTextField'),
+                style: TextStyle(
+                  color: Colors.grey.shade900,
+                  fontFamily: 'Lohit Tamil',
+                  letterSpacing: 2,
+                ),
+                controller: _sIpasswordController2,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Confirm password',
+                  hintStyle: TextStyle(
+                    color: Colors.grey.shade900,
+                    fontFamily: 'Lohit Tamil',
+                    letterSpacing: 1.5,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                      vertical: 12.0, horizontal: 12.0),
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.lock_rounded,
+                    color: Colors.grey.shade900,
+                  ),
+                  prefixIconConstraints: const BoxConstraints(
+                    minWidth: 60,
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter password';
+                  } else if (value.length < 8 || value.length > 25) {
+                    return 'Password length not between 8 and 25 chars.';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 25),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      if (_sIpasswordController.text ==
+                          _sIpasswordController2.text) {
+                        SignInAction()
+                            .signIn(
+                          _sInicknameController.text,
+                          _sIpasswordController.text,
+                          _sIemailController.text,
+                        )
+                            .then((http.Response? response) {
+                          if (response != null) {
+                            if (response.statusCode == 200) {
+                              Navigator.pop(context);
+                            } else {
+                              _sIpasswordController.clear();
+                              _sIpasswordController2.clear();
+
+                              showDialog<String>(
+                                context: context,
+                                builder: (BuildContext context) =>
+                                    ErrorDialog(prop: response.body),
+                              );
+                            }
+                          }
+                        });
+                      } else {
+                        _sIpasswordController.clear();
+                        _sIpasswordController2.clear();
+                        showDialog<String>(
+                          context: context,
+                          builder: (BuildContext context) =>
+                              const ErrorDialog(prop: 'Passwords should match'),
+                        );
+                      }
+                    }
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: Text(
+                    'Sign in',
+                    style: TextStyle(
+                      color: Colors.grey.shade900,
+                      fontFamily: 'Lohit Tamil',
+                      letterSpacing: 2,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
